44c0a56f2179348259655a4a501058f987ffb9e3
Siarhei Paulavets <s.paulavets@gmail.com>
2024-02-17 04:08:45 +0300

    Изменена привязка полей редактируемого клиента

--- WebBank/Pages/Edit.cshtml.cs
+++ WebBank/Pages/Edit.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using WebBank.AppCore.Entities;
 using WebBank.AppCore.Interfaces;
 using WebBank.Infrastructure.Data;
@@ -10,56 +11,54 @@ namespace WebBank.Pages
     public class EditModel : PageModel
     {
         private readonly IClientService _clientService;
-
-        [BindProperty]
-        public Client? Client { get; set; }
-        public SelectList Towns { get; }
-        public SelectList FamilyStatuses { get; }
-        public SelectList DisabilityGroups { get; }
-        public List<Citizenship> Citizenships { get; }
-
-        //костыли
-        [BindProperty]
-        public string BirthPlace { get; set; } = "";
-
+        private MySQLContext _context;
 
         public EditModel(MySQLContext context, IClientService clientService)
         {
             _clientService = clientService;
+            _context = context;
 
-            Towns = new SelectList(context.Towns.ToList(), "Id", "Name");
-            FamilyStatuses = new SelectList(context.FamilyStatuses.ToList(), "Id", "Name");
-            DisabilityGroups = new SelectList(context.DisabilityGroups.ToList(), "Id", "Name");
-            Citizenships = [.. context.Citizenships];
+            TownOptions = new SelectList(_context.Towns.AsNoTracking().ToList(), "Id", "Name");
+            FamilyStatusOptions = new SelectList(_context.FamilyStatuses.AsNoTracking().ToList(), "Id", "Name");
+            DisabilityGroupOptions = new SelectList(_context.DisabilityGroups.AsNoTracking().ToList(), "Id", "Name");
+            CitizenshipOptions = new SelectList(_context.Citizenships.ToList(), "Id", "Name");
         }
 
-        public IActionResult OnGet(int? id)
+        [BindProperty]
+        public Client? Client { get; set; }
+        public SelectList TownOptions { get; set; }
+        public SelectList FamilyStatusOptions { get; set; }
+        public SelectList DisabilityGroupOptions { get; set; }
+        public SelectList CitizenshipOptions { get; set; }
+
+        [BindProperty]
+        public List<int>? CitizenshipIds { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
         {
             if (id != null)
             {
-                Client = _clientService.Find(id.Value).Result;
-                if (Client != null)
-                {
-                    BirthPlace = Client.BirthPlace;
-                }
-                else
+                Client = await _clientService.Find(id.Value);
+                if (Client == null)
                     return RedirectToPage("Error");
+                CitizenshipIds = Client.Citizenships.Select(c => c.Id).ToList();
             }
             return Page();
         }
 
-        public IActionResult OnPost(int? id)
+        public async Task<IActionResult> OnPostAsync(int? id)
         {
-            if (Client != null)
+            if (Client != null && CitizenshipIds != null)
             {
-                Client.BirthPlace = BirthPlace;
+                Client.Citizenships.Clear();
+                Client.Citizenships.AddRange(CitizenshipIds!.Select(id => _context.Citizenships.First(x => x.Id == id)));
                 if (id != null)
                 {
-                    _clientService.Edit(Client);
+                    await _clientService.Edit(Client);
                 }
                 else
                 {
-                    _clientService.Add(Client);
+                    await _clientService.Add(Client);
                 }
 
                 return RedirectToPage("Index");
