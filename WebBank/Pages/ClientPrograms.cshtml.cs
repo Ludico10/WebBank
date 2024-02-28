02eb43f1426ff9e8167345cce9afd54df1223ce0
Siarhei Paulavets <s.paulavets@gmail.com>
2024-02-28 18:13:31 +0300

    Исправлен неправильный вывод страницы программ после возврата депозита

--- WebBank/Pages/ClientPrograms.cshtml.cs
+++ WebBank/Pages/ClientPrograms.cshtml.cs
@@ -8,27 +8,29 @@ using static System.Math;
 namespace WebBank.Pages;
 
 public class ClientProgramsModel(
-    MySQLContext context, 
-    IDepositService depositService, 
-    ICreditService creditService, 
+    MySQLContext context,
+    IDepositService depositService,
+    ICreditService creditService,
     ITimeService timeService) : PageModel
 {
-    private const int itemsOnPage = 10;
+    private const int itemsOnPage = 1;
 
-    public DateOnly SystemDate { get; } = timeService.GetSystemDate();
+    public DateOnly SystemDate => timeService.GetSystemDate();
     public int ClientId { get; set; }
     public string ClientName { get; set; } = "";
-    public List<ClientDeposit> Deposits { get; private set; } = [];
-    public int DepositPage { get; set; }
+    public List<ClientDeposit> Deposits => depositService.GetClientPage(ClientId, DepositPage, itemsOnPage).Result;
+    public int DepositPage { get; set; } = 1;
     public int DepositPagesCount { get; set; }
-    public List<ClientCredit> Credits { get; private set; } = [];
-    public int CreditPage { get; set; }
+    public List<ClientCredit> Credits => creditService.GetClientPage(ClientId, CreditPage, itemsOnPage).Result;
+    public int CreditPage { get; set; } = 1;
     public int CreditPagesCount { get; set; }
 
     public async Task<IActionResult> OnGetAsync(int id, int depositPage = 1, int creditPage = 1)
     {
         ClientId = id;
         DepositPage = depositPage;
+        CreditPage = creditPage;
+
         var depositsCount = await depositService.ClientDepositsCount(id);
         DepositPagesCount = (int)Max(Ceiling((double)depositsCount / itemsOnPage), 1);
 
@@ -37,7 +39,6 @@ public class ClientProgramsModel(
             return NotFound();
         }
 
-        CreditPage = creditPage;
         var creditsCount = await creditService.ClientCreditsCount(id);
         CreditPagesCount = (int)Max(Ceiling((double)creditsCount / itemsOnPage), 1);
 
@@ -53,14 +54,12 @@ public class ClientProgramsModel(
         }
         ClientName = client.Surname + " " + client.Name + " " + client.Patronymic;
 
-        Deposits = await depositService.GetClientPage(id, depositPage, itemsOnPage);
-        Credits = await creditService.GetClientPage(id, creditPage, itemsOnPage);
         return Page();
     }
 
-    public async Task<IActionResult> OnPostDeleteAsync(int id)
+    public async Task<IActionResult> OnPostDeleteAsync(int id, int depositId, int depositPage = 1, int creditPage = 1)
     {
-        var deposit = context.ClientDeposits.FirstOrDefault(cd => cd.Id == id);
+        var deposit = context.ClientDeposits.FirstOrDefault(cd => cd.Id == depositId);
         if (deposit == null)
         {
             return BadRequest();
@@ -68,6 +67,6 @@ public class ClientProgramsModel(
 
         var sysTime = new DateTime(SystemDate.Year, SystemDate.Month, SystemDate.Day, DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second);
         await depositService.Completion(deposit, sysTime);
-        return Page();
+        return RedirectToPage(new { id, depositPage, creditPage });
     }
 }
